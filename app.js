02d3edf114e0ee5574598b81806fd08d77b605dd
jeffpersonified <jeff.matthew.smith@gmail.com>
2013-01-15 13:09:18 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express')
+  , app = express()
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , port = 8080;
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || port);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+io.sockets.on('connection', function(socket){
+  console.log('Hello world!');
+});
